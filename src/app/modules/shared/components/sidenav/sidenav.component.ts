4c99cb25014843f1cc06b5c45ea40eaa888bc862
GersonVis <visoso126@gmail.com>
2023-03-07 15:53:30 -0600

    sidenav

---
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -0,0 +1,20 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent {
+
+  menuNav = [{ name: "Home", route: "home", icon: "home" },
+  { name: "Categories", route: "category", icon: "category" },
+  { name: "Productos", route: "products", icon: "production_quantity_limits" }]
+
+  mobileQuery: MediaQueryList;
+  constructor(media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  }
+
+}
